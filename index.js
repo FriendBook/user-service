72161550d85b5ddf6714b34aeba54010bb4334cf
jnavacar <jnavacar1998@gmail.com>
2022-06-07 14:53:15 +0200

    app flow rework

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const app = express();
 const PORT = 4000;
 const cors = require("cors");
 var amqp = require("amqplib/callback_api");
+var jwt = require("jsonwebtoken");
 
 const mysql = require("mysql");
 const connection = mysql.createConnection({
@@ -18,6 +19,12 @@ app.use(cors());
 connection.connect();
 app.listen(PORT, () => console.log("Listening on " + PORT));
 
+let secret = [
+  "-----BEGIN PUBLIC KEY-----",
+  "MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAi60VZKmGbOEmHJgV2nTylCNjzyLa1DRKDChAoPgWGbURzer1Ba8mivPOlxP2+wr+w/cNcagz4n+N3+03kMa7XEPhzh5C6rMQh38Dw9S43QRF3hbv88sqaweG0KvD5NOrlYLJmJ6RGb2fH6dC0IQ4JkBhtQ6Wa3kt0Omum8f7aLR5BmmEkK77/ebFtoUNPVASP9Y8LR0fO8TjcZwf6OGShI6BOYAtHdErg6lPPIzR2EYg0JR8wCT96zQv0DV9OCyaDqRXaEb2G8fatNxGOWNBG7xTxUgidNxM/BAD22DqTYXm56JF4DchSPU63Mqd3z7wsUG9KjfQSEVgPbsGhEU4cQIDAQAB",
+  "-----END PUBLIC KEY-----",
+].join("\n");
+
 var mqChannel;
 
 amqp.connect("amqp://rabbitmq:5672", function (error0, conn) {
@@ -29,9 +36,11 @@ amqp.connect("amqp://rabbitmq:5672", function (error0, conn) {
       throw error1;
     }
 
-    if (err) throw err;
+    channel.assertQueue("posts", {
+      durable: false,
+    });
 
-    channel.assertQueue("actions", {
+    channel.assertQueue("friends", {
       durable: false,
     });
 
@@ -48,23 +57,59 @@ app.get("/api/usr", (_req, res) => {
   });
 });
 
+//Check if self exists
+app.get("/api/usr/self/:id", (req, res) => {
+  jwt.verify(
+    req.headers.authorization.replace("Bearer ", ""),
+    secret,
+    { algorithms: ["RS256"] },
+    function (err, decoded) {
+      if (err) {
+        throw err;
+      }
+      connection.query(
+        `SELECT * FROM users WHERE id='${decoded.sub}'`,
+        (err, row, fields) => {
+          if (err) throw err;
+          
+          if (row.length == 0) {
+            var msg = {
+              type: "create",
+              id: decoded.sub,
+              name: decoded.preferred_username,
+            };
+            mqChannel.sendToQueue("posts", Buffer.from(JSON.stringify(msg)));
+            mqChannel.sendToQueue("friends", Buffer.from(JSON.stringify(msg)));
+
+            connection.query(
+              `INSERT INTO users (id, name, bio, birthdate) VALUES ('${
+                decoded.sub
+              }', '${decoded.preferred_username}', ${null}, ${null})`,
+              (err, _row, _fields) => {
+                if (err) throw err;
+
+                res.status(200).send(true);
+              }
+            );
+          } else {
+            res.status(200).send(false);
+          }
+        }
+      );
+    }
+  );
+});
+
 //Get a user by id
 app.get("/api/usr/:id", (req, res) => {
   connection.query(
     `SELECT * FROM users WHERE id='${req.params.id}'`,
-    (err, row, fields) => {
+    (err, row, _fields) => {
       if (err) throw err;
 
       res.status(200).send({ row });
     }
   );
-
-  // var user = users[req.params.id];
-  // if (!!user) {
-  //   res.status(200).send(user);
-  // } else {
-  //   throw new Error("User by id " + req.params.id + " does not exist.");
-  // }
 });
 
 //Create a user
@@ -74,10 +119,11 @@ app.post("/api/usr", (req, res) => {
     id: req.body.id,
     name: req.body.name,
   };
-  mqChannel.sendToQueue("actions", Buffer.from(msg));
+  mqChannel.sendToQueue("posts", Buffer.from(JSON.stringify(msg)));
+  mqChannel.sendToQueue("friends", Buffer.from(JSON.stringify(msg)));
   connection.query(
-    `INSERT INTO users (id, name, bio, birthdate) VALUES ('${req.body.id}', '${req.body.name}', '${req.body.bio}', '${req.body.birthdate}')`,
-    (err, row, fields) => {
+    `INSERT INTO users (id, name, bio, birthdate) VALUES ('${req.body.id}', '${req.body.name}', ${null}, ${null})`,
+    (err, _row, _fields) => {
       if (err) throw err;
 
       res.status(200).send();
@@ -89,13 +135,14 @@ app.post("/api/usr", (req, res) => {
 app.put("/api/usr/:id", (req, res) => {
   var msg = {
     type: "update",
-    id: req.body.id,
+    id: req.params.id,
     name: req.body.name,
   };
-  mqChannel.sendToQueue("actions", Buffer.from(msg));
+  mqChannel.sendToQueue("posts", Buffer.from(JSON.stringify(msg)));
+  mqChannel.sendToQueue("friends", Buffer.from(JSON.stringify(msg)));
   connection.query(
     `UPDATE users SET name='${req.body.name}', bio='${req.body.bio}', birthdate='${req.body.birthdate}' WHERE id='${req.params.id}'`,
-    (err, result) => {
+    (err, _result) => {
       if (err) throw err;
 
       res.status(200).send();
@@ -107,33 +154,16 @@ app.put("/api/usr/:id", (req, res) => {
 app.delete("/api/usr/:id", (req, res) => {
   var msg = {
     type: "delete",
-    id: req.body.id,
+    id: req.params.id,
   };
-  mqChannel.sendToQueue("actions", Buffer.from(msg));
+  mqChannel.sendToQueue("posts", Buffer.from(JSON.stringify(msg)));
+  mqChannel.sendToQueue("friends", Buffer.from(JSON.stringify(msg)));
   connection.query(
     `DELETE FROM users WHERE id='${req.params.id}'`,
-    (err, result) => {
+    (err, _result) => {
       if (err) throw err;
 
       res.status(200).send();
     }
   );
 });
-
-// //get for friends
-// app.get("/api/usr/frnd", (req, _res) => {
-//   var ids = req.body;
-//   var friends = [];
-
-//   for (var i = 0; i < ids.length; i++) {
-//     connection.query(
-//       `SELECT * FROM users WHERE id='${ids[i]}'`,
-//       (err, rows, fields) => {
-//         friends.push(rows.name);
-//       }
-//     ); //to be fixed
-//   }
-//   connection.query("SELECT * FROM users", (err, rows, fields) => {
-//     x;
-//   });
-// });
